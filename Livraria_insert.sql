021bc4408b92621658c775d8c23bdafb081a5e75
gpires97 <162461724+gpires97@users.noreply.github.com>
2024-09-23 11:05:43 -0300

    Add files via upload

--- Livraria_insert.sql
+++ Livraria_insert.sql
@@ -7,7 +7,13 @@ INSERT INTO LIVROS VALUES (
 34.35
 );
 
-INSERT INTO LIVROS VALUES 
-(2, 'A Volta ao mundo em 80 dias','Júlio Verne', 'Principis', 'Aventura', 21.99),
-(3, 'O Cortiço', 'Alusísio de Azevedo', 'Panda Books', 'Romance', 47.8);
+INSERT INTO LIVROS VALUES
+(2,    'A Volta ao Mundo em 80 Dias',    'Júlio Verne',         'Principis',     'Aventura',    21.99),
+(3,    'O Cortiço',                     'Aluísio de Azevedo',  'Panda Books',   'Romance',    47.8),
+(4,    'Dom Casmurro',                     'Machado de Assis',    'Via Leitura',   'Romance',    19.90),
+(5,    'Memórias Póstumas de Brás Cubas',    'Machado de Assis',    'Antofágica',    'Romance',    45),
+(6,    'Quincas Borba',                 'Machado de Assis',    'L&PM Editores', 'Romance',    48.5),
+(7,    'Ícaro',                             'Gabriel Pedrosa',     'Ateliê',          'Poesia',    36),
+(8,    'Os Lusíadas',                     'Luís Vaz de Camões',  'Montecristo',   'Poesia',    18.79),
+(9,    'Outros Jeitos de Usar a Boca',    'Rupi Kaur',          'Planeta',          'Poesia',    34.8);
 
