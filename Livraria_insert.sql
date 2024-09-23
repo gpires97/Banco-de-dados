40837f74b39d2f8bffd43ac07b0a753dde732f05
gpires97 <162461724+gpires97@users.noreply.github.com>
2024-09-23 08:45:24 -0300

    Add files via upload

---
+++ Livraria_insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO LIVROS VALUES (
+1,
+'Percy Jackson e o Ladrão de raios',
+'Rick Riordan',
+'Intrínseca',
+'Aventura',
+34.35
+);
+
+INSERT INTO LIVROS VALUES 
+(2, 'A Volta ao mundo em 80 dias','Júlio Verne', 'Principis', 'Aventura', 21.99),
+(3, 'O Cortiço', 'Alusísio de Azevedo', 'Panda Books', 'Romance', 47.8);
+
